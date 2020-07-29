f8a68e84ba368c0b2dfd32560dd8be54d0e4dcf7
keyrm10 <61849127+keyrm10@users.noreply.github.com>
2020-07-29 21:44:30 +0100

    Create launch.sh

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# terminate already running instances
+killall -q waybar
+
+# wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# launch
+waybar
